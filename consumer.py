53b537b8e0738a8e9cf820c8de9355ef38e5b569
ShedrackGodson <hedrickgodson23@gmail.com>
2020-12-31 19:05:13 +0300

    intergrated a RabbitMQ as a Messaging Broker

---
+++ consumer.py
@@ -0,0 +1,23 @@
+import pika
+
+params = pika.URLParameters("amqps://ygfxrvwk:pRe_uyBY9BgrDQMbVyYJpn8qOosuVLHx@llama.rmq.cloudamqp.com/ygfxrvwk")
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print("Received in main")
+    print(body)
+
+
+channel.basic_consume(queue='main', on_message_callback=callback)
+
+print("Started consuming")
+
+channel.start_consuming()
+
+channel.close()
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://shedd:root@db/main"
+
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', 'user_product_unique')
 
 
 @app.route('/')
